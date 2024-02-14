10fc18808885e3c41d7f9e92edf2140a1d890036
uvsingh007 <mr.uvsingh@gmail.com>
2024-02-15 00:01:28 +0530

    afadf

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const { connection } = require("./config/db");
 const { userRouter } = require("./controller/user.routes");
 const { noteRouter } = require("./controller/note.routes");
-
+const cors = require('cors')
 require("dotenv").config();
 const app = express();
-app.use(express.json());
+app.use(express.json(),cors());
 
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
